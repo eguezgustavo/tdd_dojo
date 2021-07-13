376b947fc0c0961dcd52b58dd5d5dd56012ef5e9
lenshinoda <lebps1996@gmail.com>
2021-07-13 09:22:13 -0500

    endpoint_test:sub test #2

--- example_4/api/application/endpoints/sub.py
+++ example_4/api/application/endpoints/sub.py
@@ -3,10 +3,10 @@ from flask_restx import Namespace, Resource, fields
 api = Namespace('/sub', description='Subctract two numbers')
 
 
-@api.route('/<number_1>/<number_2>')
+@api.route('/<int:number_1>/<int:number_2>')
 @api.param('number_1', 'First number')
 @api.param('number_2', 'Second number')
-@api.response(404, 'Cat not found')
+@api.response(404, 'Wrong parameters')
 class Sum(Resource):
     @api.doc('do_sum')
     def get(self, number_1, number_2):
