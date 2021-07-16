33975e30dcef3c7598af785c3bad8bdc7b8f5bc0
lenshinoda <lebps1996@gmail.com>
2021-07-16 08:57:48 -0500

    repository: added save and get methods

--- example_4/api/application/database.py
+++
@@ -1,6 +0,0 @@
-class DatabaseORM:
-    def save(self, operation):
-        pass
-
-    def query_by_id(self, id):
-        pass
---
+++ example_4/api/application/repository/operations_repository.py
@@ -0,0 +1,28 @@
+from application import db
+from .models import OperationModel
+
+class OperationsRepository:
+
+    def __init__(self) -> None:
+      self.model = OperationModel
+
+    def save(self, operation):
+      operation_object = self.model(**operation)
+      db.session.add(operation_object)
+      db.session.flush()
+      db.session.refresh(operation_object)
+      db.session.commit()
+      return operation_object.id
+      
+        
+    def query_by_id(self, id):
+      operation = self.model.query.filter(self.model.id == id).all()
+      results = [
+        {
+            "id": item.id,
+            "operation": item.operation,
+            "number1": item.number1,
+            "number2": item.number2,
+            "result": item.result,
+        } for item in operation]
+      return results
