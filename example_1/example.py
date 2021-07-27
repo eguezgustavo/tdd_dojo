5adcbf34eaa8e58cdd4b3e6b174cb1c21b0e7529
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2021-07-27 17:20:38 -0500

    sum_two_numbers sad path

--- example_1/example.py
+++ example_1/example.py
@@ -1,3 +1,8 @@
 def sum_two_numbers(number_1, number_2):
-    return number_1 + number_2
-
+    try:
+        int(number_1)
+        int(number_2)
+        return number_1 + number_2
+        
+    except ValueError:
+        return 'Values must be a numbers'
--- example_1/example_test.py
+++ example_1/example_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from example_1.example import sum_two_numbers
 
 # Happy path
@@ -8,3 +10,14 @@ def test__sum_two_numbers_function__returns_twelve__when_inputs_are_five_and_sev
     actual = sum_two_numbers(number1, number2)
 
     assert actual == 12
+
+# Sad path
+@pytest.mark.parametrize("number1,number2,expected", (
+    ('fail', 1, 'Values must be a numbers'),
+    (1, 'fail', 'Values must be a numbers'),
+))
+def test__sum_two_numbers_function__returns_error_msg__when_numbers_are_not_a_number(number1, number2, expected):
+
+    actual = sum_two_numbers(number1, number2)
+
+    assert actual == expected
