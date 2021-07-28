d0235764eae2756e7c84db0bd12eb393a1c0a57e
Jefferson Oña <67077796+jeffqev@users.noreply.github.com>
2021-07-27 19:24:40 -0500

    chage naming

--- example_1/example_test.py
+++ example_1/example_test.py
@@ -4,50 +4,50 @@ from example_1.example import fac, eval_result, sum_two_numbers, sub_two_numbers
 
 # Happy path
 def test__sum_two_numbers_function__returns_twelve__when_inputs_are_five_and_seven():
-    number1 = 5
-    number2 = 7
+    number_1 = 5
+    number_2 = 7
 
-    actual = sum_two_numbers(number1, number2)
+    actual = sum_two_numbers(number_1, number_2)
 
     assert actual == 12
 
 # Sad path
-@pytest.mark.parametrize("number1,number2,expected", (
+@pytest.mark.parametrize("number_1,number_2,expected", (
     ('fail', 1, 'Values must be a numbers'),
     (1, 'fail', 'Values must be a numbers'),
 ))
-def test__sum_two_numbers_function__returns_error_msg__when_values_are_not_a_number(number1, number2, expected):
+def test__sum_two_numbers_function__returns_error_msg__when_values_are_not_a_number(number_1, number_2, expected):
 
-    actual = sum_two_numbers(number1, number2)
+    actual = sum_two_numbers(number_1, number_2)
 
     assert actual == expected
 
 # Happy path sub_two_numbers
 def test__sub_two_numbers_function__returns_one__when_inputs_are_six_and_five():
-    number1 = 6
-    number2 = 5
+    number_1 = 6
+    number_2 = 5
 
-    actual = sub_two_numbers(number1, number2)
+    actual = sub_two_numbers(number_1, number_2)
 
     assert actual == 1
 
 # Sad path sub_two_numbers
 def test__sum_two_numbers_function__returns_error_msg__when_inputs_are_five_and_seven():
-    number1 = 5
-    number2 = 7
+    number_1 = 5
+    number_2 = 7
     expected = 'This app can only produce positive results'
 
-    actual = sub_two_numbers(number1, number2)
+    actual = sub_two_numbers(number_1, number_2)
 
     assert actual == expected
 
-@pytest.mark.parametrize("number1,number2,expected", (
+@pytest.mark.parametrize("number_1,number_2,expected", (
     ('fail', 1, 'Values must be a numbers'),
     (1, 'fail', 'Values must be a numbers'),
 ))
-def test__sub_two_numbers_function__returns_error_msg__when_values_are_not_a_number(number1, number2, expected):
+def test__sub_two_numbers_function__returns_error_msg__when_values_are_not_a_number(number_1, number_2, expected):
 
-    actual = sub_two_numbers(number1, number2)
+    actual = sub_two_numbers(number_1, number_2)
 
     assert actual == expected
 
