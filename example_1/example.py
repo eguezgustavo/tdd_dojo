dd5563fa49122cef93b8c289c9ef5108ab975d97
root <josepatricio.puebla@jharvys.com>
2022-09-19 17:32:41 +0000

    Pass the first test

---
+++ example_1/example.py
@@ -0,0 +1,3 @@
+class Calculator:
+    def add(self, first_number, second_number):
+        return 5
--- example_1/example_test.py
+++ example_1/example_test.py
@@ -1,3 +1,6 @@
+from unittest import result
+from example_1.example import Calculator
+
 def test__add__returns_five__when_first_number_is_three_and_second_one_is_two():
     # Arrange
     first_number = 3
@@ -9,3 +12,12 @@ def test__add__returns_five__when_first_number_is_three_and_second_one_is_two():
 
     # Assert
     assert result == 5
+
+def test__add__return_eight__when_first_number_is_three_and_the_second_one_is_five():
+    first_number = 3
+    second_number = 5
+
+    calculator = Calculator()
+    result = calculator.add(first_number, second_number)
+
+    assert result == 8
