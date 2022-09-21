7b0d06f70c68d231a1c1c8b8ffebb6e2a3090f49
PV ioet <paulo.viana@ioet.com>
2022-09-21 19:29:14 -0300

    Adding test for negative first number

--- example_1/example_test.py
+++ example_1/example_test.py
@@ -1,13 +1,36 @@
 from example_1.example import Calculator
 import random
+import pytest
 
 
 def test__add__returns_the_sum_of_two_numbers():
-    first_number = random.randint(0, 9)
-    second_number = random.randint(0, 9)
+    # Arrange
+    first_number = random.randint(1, 9)
+    second_number = random.randint(1, 9)
+
+    # Act
+    calculator = Calculator()
+    result = calculator.add(first_number, second_number)
+
+    # Assert
+    expected_result = first_number + second_number
+    assert result == expected_result
+
+def test__add__returns_the_same_number_if_it_is_summed_with_zero():
+    first_number = 0
+    second_number = random.randint(1, 9)
 
     calculator = Calculator()
     result = calculator.add(first_number, second_number)
 
-    expected_result = first_number + second_number
-    assert result == expected_result
+    assert result == second_number
+
+def test__add__raises_an_error_when_the_first_number_is_negative():
+    first_number = -random.randint(1, 9)
+    second_number = random.randint(1, 9)
+
+    calculator = Calculator()
+
+    with pytest.raises(ValueError) as error:
+        calculator.add(first_number, second_number)
+    assert str(error.value) == "First number shouldn't be negative"
