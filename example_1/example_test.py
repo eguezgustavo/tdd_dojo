f65bb77d86ddd4bad609aef37dfbd5f1e72cf915
PV ioet <paulo.viana@ioet.com>
2022-09-21 22:14:12 +0000

    Refactoring tests

--- example_1/example_test.py
+++ example_1/example_test.py
@@ -1,26 +1,6 @@
 from example_1.example import Calculator
 import random
 
-def test__add__returns_five__when_first_number_is_three_and_second_one_is_two():
-    # Arrange
-    first_number = 3
-    second_number = 2
-
-    # Act
-    calculator = Calculator()
-    result = calculator.add(first_number, second_number)
-
-    # Assert
-    assert result == 5
-
-def test__add__return_eight__when_first_number_is_three_and_the_second_one_is_five():
-    first_number = 3
-    second_number = 5
-
-    calculator = Calculator()
-    result = calculator.add(first_number, second_number)
-
-    assert result == 8
 
 def test__add__returns_the_sum_of_two_numbers():
     first_number = random.randint(0, 9)
